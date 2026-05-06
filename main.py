7c73907397b9dcb84243e81c4b4f8d46516a4942
JosiazFPF <josiazgpt@gmail.com>
2026-05-06 16:23:03 -0400

    initial commit

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+from playwright.sync_api import Playwright, sync_playwright, expect
 
-def main():
-    pass
 
-if __name__ == '__main__':
-    main
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://automationexercise.com/")
+    page.get_by_role("link", name=" Products").click()
+    page.get_by_role("link", name=" Home").click()
+    try:
+        expect(page.locator("iframe[name=\"aswift_5\"]").content_frame.get_by_role("button", name="Close ad")).to_be_visible()
+        page.locator("iframe[name=\"aswift_5\"]").content_frame.get_by_role("button", name="Close ad").click()
+    except:
+        pass
+
+    # --------------------- #
+    
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
